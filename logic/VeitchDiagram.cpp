e45316ca8391db129f4feea5e115ed3cd4132b63
Haris Gusic <harisgusic.dev@gmail.com>
2019-11-15 18:24:11 +0100

    Working implementation of VeitchDiagram printing

--- logic/VeitchDiagram.cpp
+++ logic/VeitchDiagram.cpp
@@ -20,6 +20,56 @@ namespace logic
 			{{}, {0, 1, 4, 5}, {1, 2, 5, 6}, {1, 2}, {0, 1, 2, 3}} //n=5
 	};
 
+	VeitchDiagram::VeitchDiagram(unsigned int nVariables) : nVariables(nVariables)
+	{
+		matrix = Matrix<unsigned short>(GetHeight(), std::vector<unsigned short>(GetWidth()));
+	}
+
+	VeitchDiagram::VeitchDiagram(unsigned int nVariables, DNF dnf, const DNF &dontCare, bool shouldMinimize)
+			: VeitchDiagram(nVariables)
+	{
+		if (shouldMinimize)
+			dnf = *GetMDNF(nVariables, dnf, dontCare).begin();
+		dnf.insert(dontCare.begin(), dontCare.end());
+		for (auto &x : dnf)
+		{
+			unsigned short term = dontCare.find(x) != dontCare.end() ? 2 : 1;
+			// Contains the rows and columns covered by this implicant
+			Contour contour = GetContour(x);
+			if (term == 1)
+				contours.push_back(contour);
+			for (auto i : contour.rows)
+				for (auto j : contour.columns)
+					matrix[i][j] = term;
+		}
+	}
+
+	VeitchDiagram::VeitchDiagram(unsigned int nVariables, DNF dnf, bool shouldMinimize) : VeitchDiagram(nVariables, dnf,
+																										{},
+																										shouldMinimize)
+	{
+	}
+
+	ullong VeitchDiagram::GetWidth() const
+	{
+		return 1ULL << ((nVariables + 1) / 2);
+	}
+
+	ullong VeitchDiagram::GetHeight() const
+	{
+		return 1ULL << (nVariables / 2);
+	}
+
+	unsigned int VeitchDiagram::GetNumberOfVariables() const
+	{
+		return nVariables;
+	}
+
+	Matrix<unsigned short> VeitchDiagram::GetMatrixRepresentation() const
+	{
+		return matrix;
+	}
+
 	// Indices: 0=A, 1=B, ...
 	std::vector<unsigned int> VeitchDiagram::GetVariableRows(unsigned int index, bool negated) const
 	{
@@ -46,19 +96,13 @@ namespace logic
 			auto columns = columnLookupTable[nVariables - 1][index];
 			if (columns.empty()) return columns;
 			std::vector<unsigned int> returnVec;
-			for (int i = 0; i < GetHeight(); ++i)
+			for (int i = 0; i < GetWidth(); ++i)
 				if (std::find(columns.begin(), columns.end(), i) == columns.end())
 					returnVec.push_back(i);
 			return returnVec;
 		}
 	}
 
-	VeitchDiagram::VeitchDiagram(unsigned int nVariables) : nVariables(nVariables)
-	{
-		matrix = Matrix<bool>(GetHeight(), std::vector<bool>(GetWidth()));
-	}
-
-	// Helper function
 	std::vector<unsigned int>
 	VeitchDiagram::GetIntersection(
 			const std::vector<unsigned int> &test, int i, const Implicant &implicant, bool rows) const
@@ -85,71 +129,179 @@ namespace logic
 		return returnVec;
 	}
 
-	VeitchDiagram::VeitchDiagram(unsigned int nVariables, const DNF &dnf) : VeitchDiagram(nVariables)
+	VeitchDiagram::Contour VeitchDiagram::GetContour(const Implicant &implicant) const
 	{
-		for (auto &x : dnf)
-		{
-			// Contain the rows and columns covered by this implicant, respectively
-			std::vector<unsigned int> rows, columns;
-			bool rowsDone = false, columnsDone = false;
-			for (int i = nVariables - 1; i >= 0; --i)
-				if (x.Contains(nVariables - 1 - i)) // Order of variables: E D C B A
-				{
-					auto testRows = GetVariableRows(i, !x[nVariables - 1 - i]);
-					if (!rowsDone && testRows.size())
-					{ // This variable is determined by a set of rows, find intersection of rows
-						rowsDone = true;
-						rows = GetIntersection(testRows, i, x, true);
-						if (columnsDone)
-							break;
-						else continue;
-					}
-					auto testColumns = GetVariableColumns(i, !x[nVariables - 1 - i]);
-					if (!columnsDone && testColumns.size())
-					{ // This variable is determined by a set of columns, find intersection of columns
-						columnsDone = true;
-						columns = GetIntersection(testColumns, i, x, false);
-						if (rowsDone)
-							break;
-						else continue;
-					}
+		Contour contour;
+		bool rowsDone = false, columnsDone = false;
+		for (int i = nVariables - 1; i >= 0; --i)
+			if (implicant.Contains(nVariables - 1 - i)) // Order of variables: E D C B A
+			{
+				auto testRows = GetVariableRows(i, !implicant[nVariables - 1 - i]);
+				if (!rowsDone && testRows.size())
+				{ // This variable is determined by a set of rows, find intersection of rows
+					rowsDone = true;
+					contour.rows = GetIntersection(testRows, i, implicant, true);
+					if (columnsDone)
+						break;
+					else continue;
 				}
-			// If rows is empty, then the implicant does not contain any variables defined by rows
-			if (rows.empty())
-			{
-				rows = std::vector<unsigned int>(GetHeight());
-				for (int i = 0; i < rows.size(); ++i)
-					rows[i] = i;
+				auto testColumns = GetVariableColumns(i, !implicant[nVariables - 1 - i]);
+				if (!columnsDone && testColumns.size())
+				{ // This variable is determined by a set of columns, find intersection of columns
+					columnsDone = true;
+					contour.columns = GetIntersection(testColumns, i, implicant, false);
+					if (rowsDone)
+						break;
+					else continue;
+				}
 			}
-			if (columns.empty())
+		// If rows is empty, then the implicant does not contain any variables defined by rows
+		if (contour.rows.empty())
+		{
+			contour.rows = std::vector<unsigned int>(GetHeight());
+			for (int i = 0; i < contour.rows.size(); ++i)
+				contour.rows[i] = i;
+		}
+		if (contour.columns.empty())
+		{
+			contour.columns = std::vector<unsigned int>(GetWidth());
+			for (int i = 0; i < contour.columns.size(); ++i)
+				contour.columns[i] = i;
+		}
+		return contour;
+	}
+
+	std::vector<VeitchDiagram::Contour> VeitchDiagram::GetContours() const
+	{
+		return contours;
+	}
+
+	bool Contains(const std::vector<unsigned int> &v, unsigned int x)
+	{
+		return std::find(v.begin(), v.end(), x) != v.end();
+	}
+
+	// Helper function
+	void Put(char &slot, char newChar)
+	{
+		if (slot != ' ' && slot != newChar)
+			slot = '+';
+		else
+			slot = newChar;
+	}
+
+	// Helper function
+	void DrawContour(std::vector<std::string> &output, const VeitchDiagram::Contour &c, int width, int height)
+	{
+		int startX = 0, startY = 0;
+		for (auto i : c.rows)
+		{
+			for (auto j : c.columns)
 			{
-				columns = std::vector<unsigned int>(GetWidth());
-				for (int i = 0; i < columns.size(); ++i)
-					columns[i] = i;
+				// x and y coordinates of the value character of the given field
+				int centerX = startX + j * 8 + 4, centerY = startY + i * 4 + 2;
+				// These determine whether there is a term above, below,... the term given by [i][j]
+				bool above = Contains(c.rows, (i + height - 1) % height) && (c.rows.size() != height || i != 0),
+						below = Contains(c.rows, (i + 1) % height) && (c.rows.size() != height || i != height - 1),
+						left = Contains(c.columns, (j + width - 1) % width) && (c.columns.size() != width || j != 0),
+						right = Contains(c.columns, (j + 1) % width) && (c.columns.size() != width || j != width - 1);
+				// Top line
+				if (!above) // There are no terms above this one
+					for (int k = -2; k <= 2; ++k)
+						Put(output[centerY - 1][centerX + k], '-');
+
+				// Bottom line
+				if (!below) // There are no terms above this one
+					for (int k = -2; k <= 2; ++k)
+						Put(output[centerY + 1][centerX + k], '-');
+
+				// Right line
+				if (!right)
+					for (int k = -1; k <= 1; ++k)
+						Put(output[centerY + k][centerX + 2], '|');
+
+				// Left line
+				if (!left)
+					for (int k = -1; k <= 1; ++k)
+						Put(output[centerY + k][centerX - 2], '|');
+				/*
+				 * Junctions (+ signs)
+				 */
+				// Above left
+				if (!above && !left)
+					output[centerY - 1][centerX - 2] = '+';
+				// Above right
+				if (!above && !right)
+					output[centerY - 1][centerX + 2] = '+';
+				// Below left
+				if (!below && !left)
+					output[centerY + 1][centerX - 2] = '+';
+				// Below right
+				if (!below && !right)
+					output[centerY + 1][centerX + 2] = '+';
+				/*
+				 * Intersections with table lines
+				 */
+				// Right intersection
+				if (right && j != width - 1)
+				{
+					if (!above)
+						for (int k = 3; k <= 4; ++k)
+							output[centerY - 1][centerX + k] = '-';
+					if (!below)
+						for (int k = 3; k <= 4; ++k)
+							output[centerY + 1][centerX + k] = '-';
+				}
+				// Left intersection
+				if (left && j != 0)
+				{
+					if (!above)
+						for (int k = 3; k <= 4; ++k)
+							output[centerY - 1][centerX - k] = '-';
+					if (!below)
+						for (int k = 3; k <= 4; ++k)
+							output[centerY + 1][centerX - k] = '-';
+				}
+				// Above intersection
+				if (above && i != 0)
+				{
+					if (!left)
+						output[centerY - 2][centerX - 2] = '|';
+					if (!right)
+						output[centerY - 2][centerX + 2] = '|';
+				}
 			}
-			for (auto i : rows)
-				for (auto j : columns)
-					matrix[i][j] = true;
 		}
 	}
 
-	ullong VeitchDiagram::GetWidth() const
+	std::ostream &operator<<(std::ostream &stream, const VeitchDiagram &diagram)
 	{
-		return 1ULL << ((nVariables + 1) / 2);
-	}
-
-	ullong VeitchDiagram::GetHeight() const
-	{
-		return 1ULL << (nVariables / 2);
-	}
-
-	unsigned int VeitchDiagram::GetNumberOfVariables() const
-	{
-		return nVariables;
-	}
-
-	Matrix<bool> VeitchDiagram::GetMatrixRepresentation() const
-	{
-		return matrix;
+		// TODO Change height & width
+		std::vector<std::string> output(4 * diagram.GetHeight() + 1, std::string(8 * diagram.GetWidth() + 1, '0'));
+		int startX = 0, startY = 0, endX = output[0].size(), endY = output.size();
+		// Filling the output with the Veitch diagram before drawing any contours.
+		for (int i = startY; i < endY; ++i)
+			for (int j = startX; j < endX; ++j)
+			{
+				if (i % 4 == 0 && j % 8 != 0)
+					output[i][j] = '-';
+				else if (j % 8 == 0 && i % 4 != 0)
+					output[i][j] = '|';
+				else if (i % 4 == 0 && j % 4 == 0)
+					output[i][j] = '+';
+				else if (i % 4 == 2 && j % 8 == 4)
+					output[i][j] =
+							diagram.matrix[i / 4][j / 8] == 2 ? 'x' : '0' + diagram.matrix[i / 4][j / 8];
+				else output[i][j] = ' ';
+			}
+		for (auto &contour : diagram.contours)
+			DrawContour(output, contour, diagram.GetWidth(), diagram.GetHeight());
+		// Sending the output to stream
+		for (int i = 0; i < output.size(); ++i)
+		{
+			if (i != 0) stream << std::string{'\n'};
+			stream << output[i];
+		}
+		return stream;
 	}
 }
--- logic/VeitchDiagram.h
+++ logic/VeitchDiagram.h
@@ -3,16 +3,31 @@
 #include <vector>
 
 #include "Implicant.h"
+#include "McCluskey.h"
 
 namespace logic
 {
+
 	/**
 	 * Currently supports at most 5 variables.
 	 */
 	class VeitchDiagram
 	{
+	public:
+		struct Contour
+		{
+			/*
+			 * The intersection of the specified rows and columns determines
+			 * where the associated Veitch diagram contains 1's.
+			 */
+			std::vector<unsigned int> rows, columns;
+		};
+	private:
+
 		unsigned int nVariables;
-		Matrix<bool> matrix;
+		// Elements equal to 0, 1, 2 represent 0's, 1's and x's respectively.
+		Matrix<unsigned short> matrix;
+		std::vector<Contour> contours;
 
 		// Private methods
 
@@ -25,11 +40,6 @@ namespace logic
 
 	public:
 
-		struct Contour
-		{
-			std::vector<unsigned int> rows, columns;
-		};
-
 		/**
 		 * Constructs a Veitch diagram for the specified number of variables
 		 */
@@ -37,25 +47,40 @@ namespace logic
 
 		/**
 		 * Constructs a Veitch diagram for the specified number of variables and
-		 * filled according to the logical function specified by \p dnf .
+		 * filled according to the logical function specified by \p dnf and \p dontCare .
+		 * Contours are constructed based on the specified form if \p shouldMinimize = <code>false</code>
+		 * and on the minimized form otherwise. If there are multiple minimal forms, the first
+		 * one returned by <code>logic::GetMDNF</code> will be used.
 		 * @param nVariables - Number of variables.
-		 * @param dnf - Disjunctive normal form of the logical function
-		 * represented by this Veitch diagram.
+		 * @param dnf - Disjunctive normal form of the logical function.
+		 * @param[optional] dontCare - Minterms that represent don't care combinations.
+		 * @param[optional] shouldMinimize - Whether the DNF specified by \p dnf has been previously minimized
+		 * represented by this Veitch diagram. Default value is <code>false</code>.
 		 */
-		explicit VeitchDiagram(unsigned int nVariables, const DNF &dnf);
+		VeitchDiagram(unsigned int nVariables, DNF dnf, const DNF &dontCare = {}, bool shouldMinimize = false);
+
+		/**
+		 * Calls <code>VeitchDiagram(nVariables, dnf, {}, shouldMinimize)</code> to construct a Veitch diagram.
+		 */
+		VeitchDiagram(unsigned int nVariables, DNF dnf, bool shouldMinimize = false);
 
 		// Getters
 
-		/** @return The number of columns of Veitch diagram. */
+		/** @return The number of columns of this Veitch diagram. */
 		ullong GetWidth() const;
 
-		/** @return The number of columns of Veitch diagram. */
+		/** @return The number of columns of this Veitch diagram. */
 		ullong GetHeight() const;
 
 		unsigned int GetNumberOfVariables() const;
 
-		Matrix<bool> GetMatrixRepresentation() const;
+		Matrix<unsigned short> GetMatrixRepresentation() const;
 
+		Contour GetContour(const Implicant &implicant) const;
+
+		std::vector<Contour> GetContours() const;
+
+		friend std::ostream &operator<<(std::ostream &stream, const VeitchDiagram &diagram);
 	};
 }
 
